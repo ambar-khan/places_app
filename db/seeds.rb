4baa495b879dc61cf986599c4247eec8fb72fb76
Ambar <ambarmogg@me.com>
2020-12-14 17:29:22 -0800

    add date via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: "The Louvre", address: "75058 Paris CEDEX 01
+  France")
+
+Place.create(name: "The Metropolitan Museum of Art", address: "1000 5th Ave, New York, NY 10028")
+
+Place.create(name: "The Getty Villa", address: "17985 Pacific Coast Hwy, Pacific Palisades, CA 90272")
+
+Place.create(name: "The British Museum", address: "Great Russell St, Bloomsbury, London WC1B 3DG, United Kingdom")
